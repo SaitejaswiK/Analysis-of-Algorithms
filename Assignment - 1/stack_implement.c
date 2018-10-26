41e58292955eb510c33fe67b94ad7277a4535292
SaitejaswiK <tejaswi.k2014@gmail.com>
2018-10-26 17:19:12 -0500

    Assignment 1

---
+++ Assignment - 1/stack_implement.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct node
+{
+    int info;
+    struct node *ptr;
+}*top,*top1,*temp;
+
+void Push(int data);
+void Pop();
+void display();
+void create();
+ 
+int count = 0;
+ 
+void main()
+{
+    int no, ch, e;
+ 
+    printf("\n 1 - Push");
+    printf("\n 2 - Pop");
+    printf("\n 3 - Exit");
+    printf("\n 4 - Dipslay");
+ 
+    create();
+ 
+    while (1)
+    {
+        printf("\n Enter choice : ");
+        scanf("%d", &ch);
+ 
+        switch (ch)
+        {
+        case 1:
+            printf("Enter data : ");
+            scanf("%d", &no);
+            Push(no);
+            break;
+        case 2:
+            Pop();
+            break;
+        case 3:
+            exit(0);
+        case 4:
+            display();
+            break;
+        default :
+            printf(" Wrong choice!!! ");
+            break;
+        }
+    }
+}
+ 
+void create()
+{
+    top = NULL;
+}
+ 
+void Push(int data)
+{
+    if (top == NULL)
+    {
+        top =(struct node *)malloc(1*sizeof(struct node));
+        top->ptr = NULL;
+        top->info = data;
+    }
+    else
+    {
+        temp =(struct node *)malloc(1*sizeof(struct node));
+        temp->ptr = top;
+        temp->info = data;
+        top = temp;
+    }
+    count++;
+}
+ 
+
+void display()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("Stack is empty");
+        return;
+    }
+ 
+    while (top1 != NULL)
+    {
+        printf("%d ", top1->info);
+        top1 = top1->ptr;
+    }
+ }
+ 
+
+void Pop()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("\n Error : Trying to pop from empty stack");
+        return;
+    }
+    else
+        top1 = top1->ptr;
+    printf("\n Popped value : %d", top->info);
+    free(top);
+    top = top1;
+    count--;
+}
